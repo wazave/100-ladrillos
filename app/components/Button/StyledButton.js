825a9ecc41701d84c1dc331eb3a9ca324f3b9cf8
Abel Elizalde <abel.elizalde@Admins-MacBook-Pro-4.local>
2018-11-01 10:51:43 -0600

    Add <Button /> component

---
+++ app/components/Button/StyledButton.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background: ${props => props.theme.colors.primary};
+
+  font-size: 16px;
+  font-family: 'Roboto Condensed', sans-serif;
+  font-stretch: condensed;
+  font-weight: 600;
+  line-height: 40px;
+  color: ${props => props.theme.colors.white};
+
+  padding: 0 24px;
+  border-radius: 20px;
+`;
+
+export default StyledButton;
---
+++ app/components/Button/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Button
+ *
+ */
+
+import React from 'react';
+
+import StyledButton from './StyledButton';
+
+function Button(props) {
+  return <StyledButton {...props} />;
+}
+
+Button.propTypes = {};
+
+export default Button;
