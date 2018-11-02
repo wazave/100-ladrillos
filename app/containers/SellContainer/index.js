465b3c760e435fd65604bb2e8dfe37502279497e
Abel Elizalde <abel.elizalde@Admins-MacBook-Pro-4.local>
2018-11-02 03:17:24 -0600

    Add <SellContainer /> translations and meta tags

---
+++ app/containers/SellContainer/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { injectIntl } from 'react-intl';
+
+import Header from 'components/Header';
+
+import messages from './messages';
+
+function SellContainer(props) {
+  return (
+    <React.Fragment>
+      <Helmet>
+        <title>{props.intl.formatMessage({ ...messages.title })}</title>
+        <meta
+          name="description"
+          content={props.intl.formatMessage({ ...messages.description })}
+        />
+      </Helmet>
+      <Header>{props.intl.formatMessage({ ...messages.header })}</Header>
+    </React.Fragment>
+  );
+}
+
+SellContainer.propTypes = {
+  intl: PropTypes.shape({ formatMessage: PropTypes.func.isRequired })
+    .isRequired,
+};
+
+export default injectIntl(SellContainer);
