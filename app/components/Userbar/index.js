ce935e6e6db5fc40194b8ee028810d2c48e1c58e
Abel Elizalde <abel.elizalde@Admins-MacBook-Pro-4.local>
2018-11-02 02:37:42 -0600

    Add <Userbar /> component

---
+++ app/components/Userbar/index.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Userbar
+ *
+ */
+
+import React from 'react';
+
+import StyledUserbar from './StyledUserbar';
+
+function Userbar(props) {
+  return <StyledUserbar {...props} />;
+}
+
+Userbar.propTypes = {};
+
+export default Userbar;
