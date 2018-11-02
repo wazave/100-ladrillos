3af0444a806148bce4da3d482b6d199c2554997f
Abel Elizalde <abel.elizalde@Admins-MacBook-Pro-4.local>
2018-11-02 02:46:42 -0600

    Add <MyAccountContainer /> component, reducer, sagas and translations

---
+++ app/containers/MyAccountContainer/HeaderButtonSeparator.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const HeaderButtonSeparator = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 16px;
+`;
+
+export default HeaderButtonSeparator;
