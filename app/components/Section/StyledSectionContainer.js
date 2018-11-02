70e198035c75f64051835ece708953f48ad37f8a
Abel Elizalde <abel.elizalde@Admins-MacBook-Pro-4.local>
2018-11-02 02:37:05 -0600

    Add <Section /> component

---
+++ app/components/Section/StyledSectionContainer.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledSectionContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 32px;
+  height: 100%;
+  width: 100%;
+  max-width: 1264px;
+`;
+
+export default StyledSectionContainer;
