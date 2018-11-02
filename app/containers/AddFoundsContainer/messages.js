85750fa761f2e00b431b0327d6143b96c7ac036c
Abel Elizalde <abel.elizalde@Admins-MacBook-Pro-4.local>
2018-11-02 03:16:52 -0600

    Add <AddFoundsContainer /> translations and meta tags

---
+++ app/containers/AddFoundsContainer/messages.js
@@ -0,0 +1,18 @@
+import { defineMessages } from 'react-intl';
+
+export const scope = 'app.containers.AddFoundsContainer';
+
+export default defineMessages({
+  header: {
+    id: `${scope}.header`,
+    defaultMessage: 'Agregar Fondos',
+  },
+  title: {
+    id: `${scope}.title`,
+    defaultMessage: 'Agregar fondos - 100 Ladrillos',
+  },
+  description: {
+    id: `${scope}.description`,
+    defaultMessage: 'Agregar Fondos a mi cuenta',
+  },
+});
