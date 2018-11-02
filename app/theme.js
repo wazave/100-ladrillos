70e384219e52a527617be143a466c5bd8e9cf834
Abel Elizalde <abel.elizalde@Admins-MacBook-Pro-4.local>
2018-11-02 02:32:30 -0600

    Add colors, elevations, shadows and transitions to main theme

--- app/theme.js
+++ app/theme.js
@@ -2,6 +2,29 @@ export default {
   colors: {
     primary: '#ff5a60',
     black: '#3e3e3e',
+    blue: '#0477c0',
+    gray: '#aaa',
+    green: '#48b83b',
+    red: '#ff5a60',
     white: '#fff',
+    background: '#f8f8f8',
+    backgroundLight: '#fbfbfb',
+    activeBackground: '#ddd',
+    divider: '#e1e1e1',
+  },
+  inherit: 'inherit',
+  none: 'none',
+  shadows: {
+    inset: 'inset 0 1px 7px 0 rgba(200, 200, 200, 0.75)',
+  },
+  transitions: {
+    deceleration: 'all 200ms cubic-bezier(0, 0, .2, 1)',
+    standard: 'all 200ms cubic-bezier(.4, 0, .2, 1)',
+    acceleration: 'all 200ms cubic-bezier(.4, 0, 1, 1)',
+    sharp: 'all 200ms cubic-bezier(.4, 0, .6, 1)',
+  },
+  elevations: {
+    1: '0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px rgba(0, 0, 0, 0.12)',
+    2: '0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 2px 2px 0px rgba(0, 0, 0, 0.12)',
   },
 };
